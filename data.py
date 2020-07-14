14983d1dc5ef76a91e7368d4f2231edcdae76d4a
Philip Hamilton <pjhamilton4@gmail.com>
2020-07-14 01:51:17 -0400

    Initial Check-in

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles =[
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum',
+            'author': 'Phil',
+            'create_date':'07-29-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum',
+            'author': 'Phil',
+            'create_date': '07-29-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum',
+            'author': 'Phil',
+            'create_date': '07-29-2019'
+        }
+    ]
+
+    return articles
